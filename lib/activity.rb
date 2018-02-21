2582f7f537a87e42ec52e2283ec9fda527d2f3e0
rcm32000 <rcm32000@yahoo.com>
2018-02-21 10:20:46 -0700

    Unable to figure this out

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+  attr_reader :name, :paid
+
+  def initialize(name=name)
+    @name = name
+    @paid = 0
+  end
+
+  # def participant
+  #   participant = []
+  #   @name << participant
+  # end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,7 +1,23 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    hiking = Activity.new
+    assert_instance_of Activity, hiking
+  end
+
+  def test_person_involvement
+    hiking = Activity.new("Rob")
+    assert_equal true, hiking.name.include?("Rob")
+  end
+
+  def test_more_than_one_person
+    hiking = Activity.new("Rob")
+    hiking = Activity.new("Margaret")
+    assert_equal true, hiking.name.include?("Rob")
+  end
 end
